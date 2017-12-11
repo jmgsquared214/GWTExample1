0a6201650f15fbcf36a95268c8d3c0d7c5e48c44
kjyothirani <katajyothi.rani@gmail.com>
2017-12-10 19:14:45 -0700

    Used DataSource instead of Spring -Hibernate

--- web/src/main/java/com/jyothi/web/model/AcademicWiki.java
+++ web/src/main/java/com/jyothi/web/model/AcademicWiki.java
@@ -8,12 +8,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import com.smartgwt.client.widgets.grid.ListGridRecord;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name="academicwiki")
-public class AcademicWiki extends ListGridRecord  implements Serializable {
+public class AcademicWiki  implements Serializable {
 	
 	/**
 	 * 
@@ -37,64 +36,65 @@ public class AcademicWiki extends ListGridRecord  implements Serializable {
 	@Column(name = "link")
 	public String link;
 	@Column(name = "pdfname")
-	public String pdf;
-	@Column(name = "created_date")
-	public String date;
+	public String pdfname;
 	@Column(name = "articlename")
-	public String articleName;
+	public String articlename;
 	@Column(name = "studentname")
-	public String studentName;
-	
-	public boolean delete;
-	
-	
-	public String getResource() {
-		return getAttributeAsString("resource");
-	}
-	public void setResource(String resource) {
-		 setAttribute("resource", resource);;
-	}
-	public String getLink() {
-		return getAttributeAsString("link");
-	}
-	public void setLink(String link) {
-		 setAttribute("link", link);;
-	}
-	public String getPdf() {
-		return getAttributeAsString("pdfname");
-	}
-	public void setPdf(String pdf) {
-		 setAttribute("pdfname", pdf);;
-	}
-	public String getDate() {
-		return getAttributeAsString("date");
-	}
-	public void setDate(String date) {
-		 setAttribute("date", date);;
-	}
-	public String getArticleName() {
-		return getAttributeAsString("articlename");
-	}
-	public void setArticleName(String articleName) {
-		 setAttribute("articlename", articleName);;
-	}
-	public String getStudentName() {
-		return getAttributeAsString("studentname");
-	}
-	public void setStudentName(String studentName) {
-		 setAttribute("studentname", studentName);;
-	}
-	public Long getId() {
-		return Long.parseLong(getAttributeAsString("id"));
-	}
-	public void setId(Long id) {
-		 setAttribute("id", id);;
-	}
-	public Boolean getDelete() {
-		return getAttributeAsBoolean("delete");
-	}
-	public void setDelete(boolean delete) {
-		 setAttribute("delete", delete);;
-	}
+	public String studentname;
+	@Column(name = "subject")
+public String 	subject;
+@Transient
+	public boolean 	Delete;
 
+
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public String getResource() {
+	return resource;
+}
+public void setResource(String resource) {
+	this.resource = resource;
+}
+public String getLink() {
+	return link;
+}
+public void setLink(String link) {
+	this.link = link;
+}
+public String getSubject() {
+	return subject;
+}
+public void setSubject(String subject) {
+	this.subject = subject;
+}
+public String getPdfname() {
+	return pdfname;
+}
+public void setPdfname(String pdfname) {
+	this.pdfname = pdfname;
+}
+public String getArticlename() {
+	return articlename;
+}
+public void setArticlename(String articlename) {
+	this.articlename = articlename;
+}
+public String getStudentname() {
+	return studentname;
+}
+public void setStudentname(String studentname) {
+	this.studentname = studentname;
+}
+public boolean isDelete() {
+	return Delete;
+}
+public void setDelete(boolean delete) {
+	Delete = delete;
+}
+	
+	
 }
--- web/src/main/java/com/jyothi/web/parent.java
+++ web/src/main/java/com/jyothi/web/parent.java
@@ -1,33 +1,20 @@
 package com.jyothi.web;
-import java.util.List;
 import java.util.logging.Logger;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.jyothi.web.AcademicWikiModel;
-import com.jyothi.web.AcademicWikiService;
-import com.jyothi.web.AcademicWikiServiceAsync;
-import com.jyothi.web.model.AcademicWiki;
-import com.smartgwt.client.core.DataClass;
-import com.smartgwt.client.data.Criteria;
 import com.smartgwt.client.data.DataSource;
 import com.smartgwt.client.data.DataSourceField;
-import com.smartgwt.client.data.Record;
-import com.smartgwt.client.types.DSDataFormat;
 import com.smartgwt.client.types.FetchMode;
 import com.smartgwt.client.types.FieldType;
-import com.smartgwt.client.types.ListGridFieldType;
 import com.smartgwt.client.widgets.Canvas;
 import com.smartgwt.client.widgets.IButton;
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
 import com.smartgwt.client.widgets.grid.ListGrid;
-import com.smartgwt.client.widgets.grid.ListGridField;
-import com.smartgwt.client.widgets.grid.ListGridRecord;
-import com.smartgwt.client.widgets.grid.events.RecordClickEvent;
-import com.smartgwt.client.widgets.grid.events.RecordClickHandler;
+import com.smartgwt.client.widgets.grid.ListGridRecord; 
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -64,181 +51,68 @@ public class parent implements EntryPoint {
 	 final ListGrid listGrid = new ListGrid();  
 	  listGrid.setWidth(900);  
 	  listGrid.setHeight(224);  
-	  listGrid.setAlternateRecordStyles(true);  
-	  listGrid.setCanEdit(false);  
-	  listGrid.setCanRemoveRecords(true);  
+	  listGrid.setCanRemoveRecords(false);  
 	  listGrid.setDataFetchMode(FetchMode.LOCAL);  
-	  listGrid.setAutoFetchData(false);  
-	  listGrid.setUseAllDataSourceFields(true);  
-	  listGrid.setShowFilterEditor(true);  
-	  listGrid.setAutoFetchData(false);
 	  listGrid.setCanEdit(true);
 	  listGrid.setAutoSaveEdits(false);
 	  listGrid.setSaveByCell(false);
-	  
-	  ListGridField resourceField = new ListGridField("resource", "Resource", 120);
-	  ListGridField linkField = new ListGridField("link", "Link");
-	  ListGridField pdfnameField = new ListGridField("pdfname", "PdfName", 120);
-	  ListGridField articlenameField = new ListGridField("articlename", "ArticleName");
-	  ListGridField studentnameField = new ListGridField("studentname", "StudentName");
-	  ListGridField checkBoxField = new ListGridField("delete");
-	  checkBoxField.setType(ListGridFieldType.BOOLEAN);
-	  checkBoxField.setCanToggle(true);
-	  checkBoxField.setCanEdit(true);
-	  listGrid.setFields(new ListGridField[] {resourceField, linkField, pdfnameField,
-    		  articlenameField, studentnameField,checkBoxField});
-	
+	 
+	  DataSource dataSource = DataSource.get("contacts");
+	  listGrid.setDataSource(dataSource);
+	  listGrid.setAutoFetchData(true);
 	    Canvas canvas = new Canvas();
-	    canvas.addChild(listGrid);
-	    IButton button = new IButton("Add New");
-	    button.setTop(250);
-	    button.addClickHandler(new ClickHandler() {
-	        public void onClick(ClickEvent event) {
-	        	Record record = new Record();
-	        	record.setAttribute("resource", "");
-	        	record.setAttribute("link", "");
-	        	record.setAttribute("pdfname", "");
-	        	record.setAttribute("articlename", "");
-	        	record.setAttribute("studentname", "");
-	        	listGrid.startEditingNew(record);
-	        }
-	    });
-	    IButton saveButton = new IButton("Delete");
-	    saveButton.setTop(250);
-	    saveButton.setLeft(250);
-	    saveButton.addClickHandler(new ClickHandler() {
-	        public void onClick(ClickEvent event) {
-	        	listGrid.saveAllEdits();
-	        	  for(ListGridRecord records: listGrid.getRecords())
-		       	   {
-	        		  boolean checked = records.getAttributeAsBoolean("delete");
-	        		  consoleLog(" Delete"+checked);
-	        		  if(checked)
-	        		  {
-	        			Long id=Long.parseLong(  records.getAttributeAsString("id"));
-	        			consoleLog(" Deleteid"+id);
-	        			GWT.log(id+"");
-	        			academicWikiService.delete(id, new AsyncCallback<Void>() {
-							
-							@Override
-							public void onSuccess(Void result) {
-								// TODO Auto-generated method stub
-								consoleLog(" DeleteidSuccess");
-							}
-							
-							@Override
-							public void onFailure(Throwable caught) {
-								// TODO Auto-generated method stub
-								consoleLog(" DeleteidFail");
-							}
-						}) ;
-	        		  }
-		       	   }
-	        	  
-	        	  for(ListGridRecord records: listGrid.getRecords())
-		       	   {
-	        		  boolean checked = records.getAttributeAsBoolean("delete");
-	        		  consoleLog(" Delete"+checked);
-	        		  if(checked)
-	        		  {
-	        			  listGrid.removeData(records);
-	        		  }
-		       	   }
-	        	
-	        }
-	    });
+	    IButton newButton = new IButton("Add New");  
+	    newButton.setTop(250);
 	    
-	    IButton deleteButton = new IButton("Save");
-	    deleteButton.setTop(250);
-	    deleteButton.setLeft(110);
-	    deleteButton.addClickHandler(new ClickHandler() {
+	    IButton saveButton = new IButton("Save");
+	    saveButton.setTop(250);
+	    saveButton.setLeft(110);
+        newButton.addClickHandler(new ClickHandler() {  
+            public void onClick(ClickEvent event) {  
+                listGrid.startEditingNew();  
+            }  
+        });  
+        
+        saveButton.addClickHandler(new ClickHandler() {
 	        public void onClick(ClickEvent event) {
 	        	GWT.log("Enteredsssds");listGrid.saveAllEdits();
 	        	  for(ListGridRecord records: listGrid.getRecords())
 	       	   {
 	        		  GWT.log("Enteredsssds");
-	        		  AcademicWikiModel model = new AcademicWikiModel();
 	        		 String id= records.getAttributeAsString("id");
 	        		 if(null==id){
-	       		  model.setResource(records.getAttributeAsString("resource"));
-	       		model.setLink(records.getAttributeAsString("link"));
-	       		model.setPdf(records.getAttributeAsString("pdfname"));
-	       		
-	       		model.setArticleName(records.getAttributeAsString("articlename"));
-	       		model.setStudentName(records.getAttributeAsString("studentname"));
-	       		academicWikiService.saveOrUpdate(model, new AsyncCallback<Void>() {
-					
-					@Override
-					public void onSuccess(Void result) {
-						  GWT.log("Success");
-						
-					}
-					
-					@Override
-					public void onFailure(Throwable caught) {
-						  GWT.log("failure");
-						
-					}
-				});
+	        			 dataSource.addData(records);
 	        		 }
 	       	   }
-	        	 
 	        }
 	    });
-	    
-	    listGrid.addRecordClickHandler(new RecordClickHandler() {
-	        @Override
-	        public void onRecordClick(RecordClickEvent event) {
-	            // your code
-	        	 GWT.log("Entered1");
+        IButton deleteButton = new IButton("Delete");
+        deleteButton.setTop(250);
+        deleteButton.setLeft(250);
+        deleteButton.addClickHandler(new ClickHandler() {
+	        public void onClick(ClickEvent event) {
+	        	  for(ListGridRecord records: listGrid.getSelectedRecords())
+		       	   {
+	        		  boolean checked = records.getAttributeAsBoolean("Delete");
+	        		  consoleLog(" Delete"+checked);
+	        			  dataSource.removeData(records);
+		       	   }
 	        }
-	});
-	    
-	    AsyncCallback<List<AcademicWikiModel>> modelObject= new AsyncCallback<List<AcademicWikiModel>>() {
-
-			@Override
-			public void onFailure(Throwable caught) {
-				AcademicWiki model = new AcademicWiki();
-				model.setLink("failed");
-				 AcademicWiki[] modelArray = new AcademicWiki[1];
-				modelArray[0]=model;
-			      listGrid.setData(modelArray);
-			}
-
-			@Override
-			public void onSuccess(List<AcademicWikiModel> result) {
-				AcademicWiki[] modelArray = new AcademicWiki[result.size()];
-				int i=0;
-				for(AcademicWikiModel model: result)
-				{
-				AcademicWiki model1 = new AcademicWiki();
-				model1.setLink(model.getLink());
-				model1.setResource(model.getResource());
-				model1.setStudentName(model.getStudentName());
-				model1.setArticleName(model.getArticleName());
-				consoleLog(model.getPdf());
-				model1.setPdf(model.getPdf());
-				consoleLog("Id is"+model.getId());
-				model1.setId(model.getId());
-				model1.setDelete(false);
-				modelArray[i]=model1;
-				i++;
-			     
-				}
-			     
-				 listGrid.setData(modelArray);
-			}
-		};
-		
-		academicWikiService.findAllEntries(modelObject);
-	    canvas.addChild(button);
-	  
+	    });
+        
+	    canvas.addChild(listGrid);
+	    canvas.addChild(newButton);
 	    canvas.addChild(saveButton);
-	    
 	    canvas.addChild(deleteButton);
+		 
 	   
 	    RootPanel.get().add(canvas);
   }
+ 
+ public void onFailure(Throwable caught) {
+     // Show the RPC error message to the user
+    GWT.log("error",caught);
+   }
  
  native void consoleLog( String message) /*-{
  console.log( "me:" + message );
